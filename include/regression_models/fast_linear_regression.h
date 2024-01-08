7eb68b4b442c8c8f6fa78ee1386cd822d0a4be3c
Sarem Seitz <sarem.seitz@gmail.com>
2024-01-08 10:07:26 +0100

    fast linear regression

---
+++ include/regression_models/fast_linear_regression.h
@@ -0,0 +1,38 @@
+#ifndef FAST_LINEAR_REGRESSION_H
+#define FAST_LINEAR_REGRESSION_H
+
+#include "linalg.h"
+#include "regression_model.h"
+
+//fast updates for linear regression (currently only supports 1 feature)
+class FastLinearRegression: public RegressionModel {
+private:
+	double beta0;
+	double beta1;
+
+	double sum_xy;
+	double sum_x;
+	double sum_y;
+	double sum_x_sq; //sum of x^2
+	double sum_sq_x; //sum of x, squared
+	
+	double n_obs;
+
+	double lambda_regularization;
+	bool is_trained;
+public:
+	FastLinearRegression(double lambda_regularization = 0.0):
+		lambda_regularization(lambda_regularization),
+		is_trained(false) {};
+
+	void fit(const Matrix &X, const Matrix &y);
+
+	//update coefficients by adding more data
+	void update_coefficients_add(const Matrix &X, const Matrix &y);
+	//update coefficients by dropping data
+	void update_coefficients_drop(const Matrix &X, const Matrix &y);
+	Matrix predict(const Matrix &X) const;
+	Matrix get_coefficients() const;
+};
+
+#endif
--- tests/unittest_linear_regression_model.cpp
+++ tests/unittest_linear_regression_model.cpp
@@ -1,8 +1,10 @@
 #include <vector>
+#include <iostream>
 
 #include "gtest/gtest.h"
 #include "linalg.h"
 #include "regression_models/linear_regression_model.h"
+#include "regression_models/fast_linear_regression.h"
 
 TEST(testregression, simple_regression) {
 	std::vector<double> inputs = {
@@ -11,9 +13,9 @@ TEST(testregression, simple_regression) {
 		4.0,
 		5.0,
 		6.0,
-		7,0,
-		8,0,
-		9,0,
+		7.0,
+		8.0,
+		9.0,
 		10.0,
 		11.0
 	};
@@ -32,3 +34,481 @@ TEST(testregression, simple_regression) {
 	EXPECT_TRUE(mse < 1e-6);
 }
 
+TEST(testregression, fast_regression) {
+	std::vector<double> inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+	Matrix X = Matrix(inputs);
+
+	Matrix y = Matrix(inputs);
+
+	FastLinearRegression model = FastLinearRegression();
+	model.fit(X,y);
+
+	Matrix predictions = model.predict(X);
+	
+	double mse = (predictions - y).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, fast_regression_constant_ones) {
+	std::vector<double> inputs = {
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0
+	};
+
+	std::vector<double> targets = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	Matrix X = Matrix(inputs);
+	Matrix y = Matrix(targets);
+
+	FastLinearRegression model = FastLinearRegression();
+	model.fit(X,y);
+
+	Matrix predictions = model.predict(X);
+	
+ 	double y_mean = y.sum() / 10.0;	
+	Matrix y_mean_mat = Matrix(std::vector<double>(10, y_mean));
+
+
+	double mse = (predictions - y_mean_mat).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, comp_normal_fast_regression) {
+	std::vector<double> inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+	Matrix X = Matrix(inputs);
+	Matrix Xnormal = Matrix(inputs);
+	Xnormal.prepend_ones();
+
+	Matrix y = Matrix(inputs);
+
+	LinearRegressionModel normal_model = LinearRegressionModel();
+	FastLinearRegression fast_model = FastLinearRegression();
+	
+	normal_model.fit(Xnormal,y);
+	
+	fast_model.fit(X,y);
+
+
+	Matrix predictions_normal = normal_model.predict(Xnormal);
+
+
+	Matrix predictions_fast = fast_model.predict(X);
+
+	double mse = (predictions_normal - predictions_fast).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, comp_regularized_normal_fast_regression) {
+	std::vector<double> inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+	Matrix X = Matrix(inputs);
+	Matrix Xnormal = Matrix(inputs);
+	Xnormal.prepend_ones();
+
+	Matrix y = Matrix(inputs);
+
+	LinearRegressionModel normal_model = LinearRegressionModel(5.0);
+	FastLinearRegression fast_model = FastLinearRegression(5.0);
+	
+	normal_model.fit(Xnormal,y);
+	
+	fast_model.fit(X,y);
+
+
+	Matrix predictions_normal = normal_model.predict(Xnormal);
+
+
+	Matrix predictions_fast = fast_model.predict(X);
+
+	double mse = (predictions_normal - predictions_fast).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+TEST(testregression, fast_regression_add) {
+	std::vector<double> full_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> full_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0,
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	std::vector<double> first_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0
+	};
+	
+	std::vector<double> first_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0
+	};
+
+	std::vector<double> second_inputs = {
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> second_targets = {
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	Matrix X = Matrix(full_inputs);
+	Matrix y = Matrix(full_targets);
+	Matrix X1 = Matrix(first_inputs);
+	Matrix y1 = Matrix(first_targets);
+	Matrix X2 = Matrix(second_inputs);
+	Matrix y2 = Matrix(second_targets);
+
+	FastLinearRegression model = FastLinearRegression();
+	model.fit(X1,y1);
+	model.update_coefficients_add(X2,y2);
+
+	Matrix predictions = model.predict(X);
+
+	FastLinearRegression model2 = FastLinearRegression();
+	model2.fit(X,y);
+
+	Matrix predictions2 = model2.predict(X);
+
+	double mse = (predictions - predictions2).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, fast_regression_add_regularized) {
+	std::vector<double> full_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> full_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0,
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	std::vector<double> first_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0
+	};
+	
+	std::vector<double> first_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0
+	};
+
+	std::vector<double> second_inputs = {
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> second_targets = {
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	Matrix X = Matrix(full_inputs);
+	Matrix y = Matrix(full_targets);
+	Matrix X1 = Matrix(first_inputs);
+	Matrix y1 = Matrix(first_targets);
+	Matrix X2 = Matrix(second_inputs);
+	Matrix y2 = Matrix(second_targets);
+
+	FastLinearRegression model = FastLinearRegression(5.0);
+	model.fit(X1,y1);
+	model.update_coefficients_add(X2,y2);
+
+	Matrix predictions = model.predict(X);
+
+	FastLinearRegression model2 = FastLinearRegression(5.0);
+	model2.fit(X,y);
+
+	Matrix predictions2 = model2.predict(X);
+
+	double mse = (predictions - predictions2).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, fast_regression_drop) {
+	std::vector<double> full_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> full_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0,
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	std::vector<double> first_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0
+	};
+	
+	std::vector<double> first_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0
+	};
+
+	std::vector<double> second_inputs = {
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> second_targets = {
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	Matrix X = Matrix(full_inputs);
+	Matrix y = Matrix(full_targets);
+	Matrix X1 = Matrix(first_inputs);
+	Matrix y1 = Matrix(first_targets);
+	Matrix X2 = Matrix(second_inputs);
+	Matrix y2 = Matrix(second_targets);
+
+	FastLinearRegression model = FastLinearRegression();
+	model.fit(X,y);
+	model.update_coefficients_drop(X2,y2);
+
+	Matrix predictions = model.predict(X);
+
+	FastLinearRegression model2 = FastLinearRegression();
+	model2.fit(X1,y1);
+
+	Matrix predictions2 = model2.predict(X);
+
+	double mse = (predictions - predictions2).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
+
+
+TEST(testregression, fast_regression_drop_regularized) {
+	std::vector<double> full_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0,
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> full_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0,
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	std::vector<double> first_inputs = {
+		2.0,
+		3.0,
+		4.0,
+		5.0,
+		6.0
+	};
+	
+	std::vector<double> first_targets = {
+		2.0,
+		2.0,
+		6.4,
+		5.0,
+		6.0
+	};
+
+	std::vector<double> second_inputs = {
+		7.0,
+		8.0,
+		9.0,
+		10.0,
+		11.0
+	};
+
+	std::vector<double> second_targets = {
+		7.0,
+		8.2,
+		9.0,
+		11.0,
+		11.0
+	};
+
+	Matrix X = Matrix(full_inputs);
+	Matrix y = Matrix(full_targets);
+	Matrix X1 = Matrix(first_inputs);
+	Matrix y1 = Matrix(first_targets);
+	Matrix X2 = Matrix(second_inputs);
+	Matrix y2 = Matrix(second_targets);
+
+	FastLinearRegression model = FastLinearRegression(5.0);
+	model.fit(X,y);
+	model.update_coefficients_drop(X2,y2);
+
+	Matrix predictions = model.predict(X);
+
+	FastLinearRegression model2 = FastLinearRegression(5.0);
+	model2.fit(X1,y1);
+
+	Matrix predictions2 = model2.predict(X);
+
+	double mse = (predictions - predictions2).pow_elementwise(2.0).sum() / 10.0;
+
+	EXPECT_TRUE(mse < 1e-6);
+}
